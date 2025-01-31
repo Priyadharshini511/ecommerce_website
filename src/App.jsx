c552bc9dbec112341f86d85ee7ef268429b87c65
Priyadharshini511 <153971950+Priyadharshini511@users.noreply.github.com>
2025-01-31 14:43:19 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
+import './index.css';
+import { Button } from "./components/ui/button";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminFeatures from "./pages/admin-view/features";
+import ShoppingLayout from "./components/shopping-view/layout";
+import NotFound from "./components/not-found";
+import ShoppingHome from "./pages/shopping-view/home";
+import Shoppinglisting from "./pages/shopping-view/listing";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
+import ShoppingAccount from "./pages/shopping-view/account";
+import CheckAuth from "./components/common/check-auth";
+import UnauthPage from "./pages/unauth-page";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { checkAuth } from "./store/auth-slice";
+import { Skeleton } from "@/components/ui/skeleton";
+import PaypalReturnPage from "./pages/shopping-view/paypal-return";
+import PaymentSuccessPage from "./pages/shopping-view/paypal-success";
+
+function App() {
+  const { user, isAuthenticated,  isLoading  } = useSelector(
+    (state) => state.auth
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+  
+  if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
+
+  console.log(isLoading, user);
+
+  return (
+    
+      <div className="flex flex-col overflow-hidden bg-white">
+          {/* Common Header */}
+        
+
+           {/* Define Routes */}
+          <Routes>
+             <Route path="/auth" element={
+               <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                 <AuthLayout/>
+                 </CheckAuth>
+               }>
+               <Route path="login" element={<AuthLogin />} />
+               <Route path="register" element={<AuthRegister />} />
+             </Route>
+             <Route path="/admin" element={
+                <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                 <AdminLayout/>
+                </CheckAuth>}>
+             <Route path="dashboard" element={<AdminDashboard/>} />
+             <Route path="products" element={<AdminProducts />} />
+             <Route path="orders" element={<AdminOrders />} />
+             <Route path="features" element={<AdminFeatures />} />
+             </Route>
+             <Route path="/shop" element={
+                <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+                 <ShoppingLayout/>
+                </CheckAuth>}>
+             <Route path="home" element={<ShoppingHome/>}/>
+             <Route path="listing" element={<Shoppinglisting/>}/>
+             <Route path="checkout" element={<ShoppingCheckout/>}/>
+             <Route path="account" element={<ShoppingAccount/>}/>
+             <Route path="paypal-return" element={<PaypalReturnPage />} />
+             <Route path="payment-success" element={<PaymentSuccessPage />} />
+             </Route>
+              <Route path="*" element={<NotFound/>}/>
+              <Route path="/unauth-page" element={<UnauthPage/>}/>
+
+              
+            
+           </Routes>
+      
+      </div>
+    
+  );
+}
+
+export default App;
+
+//2.57.21
---
+++ src/pages/admin-view/features.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+function AdminFeatures() {
+  return (
+    <div>  Admin Features</div>
+  )
+}
+
+export default AdminFeatures
